c892277790f45ecf8c6bf4b16c2e9c9eb69e3d8f
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2025-03-06 21:39:21 +0100

    Add Redis caching to TypeORM configuration
    Integrate Redis as the caching mechanism for TypeORM with configurable options such as host, port, and password. Update `package.json` dependencies to include `redis` and its type definitions. Extend environment variable schema and docker-compose configuration to support Redis settings.

--- apps/backend/src/configs/typeorm.ts
+++ apps/backend/src/configs/typeorm.ts
@@ -8,6 +8,17 @@ export const typeormConfig: DataSourceOptions = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  cache: {
+    type: 'redis',
+    options: {
+      socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+        password: process.env.REDIS_PASSWORD,
+      },
+    },
+    duration: 30000,
+  },
 };
 
 export const typeormSchema = {
@@ -16,4 +27,7 @@ export const typeormSchema = {
   POSTGRES_USER: Joi.string().required(),
   POSTGRES_PASSWORD: Joi.string().required(),
   POSTGRES_DB: Joi.string().required(),
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.string().required(),
+  REDIS_PASSWORD: Joi.string().optional(),
 };
